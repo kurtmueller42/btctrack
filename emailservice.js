e0e40b52b0d2c0a3a1cd9b2a5f7c49d3012a7e73
km42 <kurtmueller42@gmail.com>
2018-06-03 22:43:16 -0400

    Added emailing

--- emailservice.js
+++ emailservice.js
@@ -1,11 +1,40 @@
+var nodemailer = require('nodemailer');
+
+// Pass these by environment variable I guess?
+// WARNING: only do this with real credentials if you've locked down your server
+var email = process.env.EMAIL;
+var pass = process.env.EMAILPASS;
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: pass
+    }
+  });
 
 
+async function sendAlert(destEmail, oldPrice, newPrice) {
+    var mailOptions = {
+        from: email,
+        to: destEmail,
+        subject: 'BTC price change alert',
+        text: 'Bitcoin has changed in price from ' + oldPrice + ' to ' + newPrice + '.'
+      };      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email successfully sent to ' + destEmail);
+        }
+      });      
+}
+  
 module.exports = {
 
     // The alert object contains the threshold, email address, and direction of the threshold
     TriggerThresholdEmail: function(oldPrice, newPrice, alert) {
-        // TODO
-        // For now console log
-        console.log('This is an alert for ' + alert.email_address + ' that the price changed from ' + oldPrice + ' to ' + newPrice);
+        console.log('Sending an alert to ' + alert.email_address);
+        sendAlert(alert.email_address, oldPrice, newPrice);
     }
 }
